3f6a2a4b54f995b9f8898e74e87318953d2fba6b
shubeen <67527789+shubeen@users.noreply.github.com>
2020-09-13 00:30:01 +1000

    first commit

---
+++ src/components/Login/firebase.Config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7gZUWzQjqt_9qCvwy_LmBvtzOYXToByU",
+    authDomain: "burj-al-arab-3f80e.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-3f80e.firebaseio.com",
+    projectId: "burj-al-arab-3f80e",
+    storageBucket: "burj-al-arab-3f80e.appspot.com",
+    messagingSenderId: "284701408342",
+    appId: "1:284701408342:web:ce7505cccecacd681d4ff7"
+  };
+  export default firebaseConfig;
